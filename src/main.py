0bd735bed4b91e7b72299592b288f047b5b2a4ee
songyz2019 <songyz2023dlut@outlook.com>
2024-12-29 19:09:22 +0800

    update src

--- src/main.py
+++ src/main.py
@@ -4,41 +4,63 @@ import skimage
 from hsi2rgb import hsi2rgb
 from scipy.io import loadmat
 from einops import rearrange
+import logging
+import json
 
-def hsi2rgb_app(hsi_file, wavelength_from=380, wavelength_to=1050, gamma=1/1.5):
+logger = logging.getLogger(__name__)
 
-    match = hsi_file.split('.')[-1]
-    if match == 'mat':
+
+def hsi2rgb_app(hsi_file, wavelength_from=380, wavelength_to=1050, gamma=1/1.5, is_chw_input=False):
+    ext = hsi_file.split('.')[-1]
+    if ext == 'mat':
         mat = loadmat(
             hsi_file,
             squeeze_me=True,
             mat_dtype=True,
-            # struct_as_record=False
+            struct_as_record=False
         )
         key = list(mat.keys())[3]
         hsi = mat[key]
-    elif match in ['tiff','tif']:
+
+        # For capbility of reading .mat file with different structure, like the offical MUUFL dataset
+        if not isinstance(hsi, np.ndarray):
+            hsi = hsi.Data
+        
+    elif ext in ['tiff','tif']:
         hsi = skimage.io.imread(hsi_file)
     else:
         return
+
+    if is_chw_input:
+        hsi = rearrange(hsi, 'c h w -> h w c')
 
     n_channel = hsi.shape[-1]
     wavelength = np.linspace(wavelength_from, wavelength_to, n_channel)
     rgb = hsi2rgb(hsi, wavelength, raw=False) 
     
     img = skimage.exposure.adjust_gamma(rgb, gamma)
-    return img
-    
 
-demo = gr.Interface(
-    fn=hsi2rgb_app,
-    inputs=[
-        gr.File(file_types=['.mat','.tiff','.tif'], type='filepath'), 
-        gr.Number(380), 
-        gr.Number(1050), 
-        gr.Slider(0.5, 2, 1/1.5)
-    ],
-    outputs=[gr.Image()],
-)
+    extra = json.dumps({
+        'n_channel': n_channel,
+        'mat_file_key': key if ext == 'mat' else None,
+    })
+    return img, extra
 
-demo.launch(share=True)
+def main():  
+    demo = gr.Interface(
+        fn=hsi2rgb_app,
+        inputs=[
+            gr.File(file_types=['.mat','.tiff','.tif'], type='filepath'), 
+            gr.Number(380), 
+            gr.Number(1050), 
+            gr.Slider(0.5, 2, 1/1.5),
+            gr.Checkbox(False, label='Is CHW input?', show_label=True)
+        ],
+        outputs=[gr.Image(label='RGB Image'), gr.Textbox()],
+    )
+
+    demo.launch(share=True)
+
+if __name__ == '__main__':
+    logger.setLevel(logging.INFO)
+    main()
